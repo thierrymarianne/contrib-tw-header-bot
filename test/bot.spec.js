31ae2a85b5b1616e55fe2839be72348c7a2c9a59
Thierry Marianne <thierry@marianne.io>
2022-08-22 16:57:39 +0200

    Add test workflow

---
+++ test/bot.spec.js
@@ -0,0 +1,8 @@
+import Bot from '../index.js'
+import mockAxios from 'jest-mock-axios';
+
+describe('Twitter Header Bot', () => {
+    it('runs', () => {
+        expect(true).toEqual(true);
+    });
+});
